909fd20429a48739923eb6d19a991c7763c19fe2
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2024-10-15 17:14:00 +0200

    refactored the documentation

--- GMB-AssIoT1/include/timers.h
+++ GMB-AssIoT1/include/timers.h
@@ -1,9 +1,27 @@
+/*
+    It is the starting time of the game.
+*/
 #define START_TIME_SEC 60UL
-#define MICRO_MUL 1000000UL
+
+/*
+    It is the time that the game will sleep during the blinking state.
+*/
 #define GOTO_SLEEP_TIME 10UL
-#define F_FACTOR 2UL
+
+/*
+    It is the minimum time that the game will have.
+*/
 #define MIN_TIME_VAL 20UL
 
+/*
+    It is a multiplier to convert seconds to microseconds.
+*/
+#define MICRO_MUL 1000000UL
+
+/*
+    It is the factor that will be multiplied by the difficulty.
+*/
+#define F_FACTOR 2UL
 
 /*
     Sets up the timer with the sleeping time in the setup.
