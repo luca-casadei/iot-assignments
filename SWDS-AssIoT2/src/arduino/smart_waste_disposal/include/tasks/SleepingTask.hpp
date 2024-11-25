3405a24614606165e6588f664f3ec981cd6a2daa
Luca Casadei <luca.casadei27@studio.unibo.it>
2024-11-25 14:07:08 +0100

    Completed the sleeping task with every behaviour

--- SWDS-AssIoT2/src/arduino/smart_waste_disposal/include/tasks/SleepingTask.hpp
+++ SWDS-AssIoT2/src/arduino/smart_waste_disposal/include/tasks/SleepingTask.hpp
@@ -1,21 +1,22 @@
 #pragma once
 #include "TaskWithCounter.hpp"
 
+class LCD;
+
 class SleepingTask : public TaskWithCounter
 {
 
 private:
   unsigned int motionDetectorPin;
   unsigned int counter;
-  unsigned int time_before_sleeping;
-  unsigned int seconds_before_sleeping;
+  LCD * lcd;
   enum {
     AWAKE,
     SLEEPING
   } state;
 
 public:
-  SleepingTask(unsigned int motionDetectorPin, unsigned int seconds_before_sleeping);
-  virtual void init(unsigned int period) final;
-  virtual void tick() final;
+  SleepingTask(unsigned int motionDetectorPin, unsigned int seconds_before_sleeping, LCD * lcd);
+  void init(unsigned int period) final;
+  void tick() final;
 };
--- SWDS-AssIoT2/src/arduino/smart_waste_disposal/src/main.cpp
+++ SWDS-AssIoT2/src/arduino/smart_waste_disposal/src/main.cpp
@@ -3,6 +3,7 @@
 #include "tasks/SleepingTask.hpp"
 #include <Arduino.h>
 #include "components/Led.hpp"
+#include "components/LCD.hpp"
 
 // #define TEST_COMPONENTS
 
@@ -20,14 +21,19 @@ void setup()
     tester->init();
     MsgService.init();
 #else
-//Tasks
-//Sleeping task
-Task * sleep = new SleepingTask(MOTION_DETECTOR, 5);
-sleep->init(TASK_SLEEP_PERIOD);
+    Serial.begin(BAUD);
+    // Components
+    pinMode(MOTION_DETECTOR, INPUT);
+    LCD *lcd = new LCD(LCD_ADDR, LCD_COLS, LCD_ROWS);
+    lcd->init();
+    // Tasks
+    // Sleeping task
+    Task *sleep = new SleepingTask(MOTION_DETECTOR, 5, lcd);
+    sleep->init(TASK_SLEEP_PERIOD);
 
-//Scheduler
-scheduler.init(SCHEDULER_MCD_PERIOD);
-scheduler.addTask(sleep);
+    // Scheduler
+    scheduler.init(SCHEDULER_MCD_PERIOD);
+    scheduler.addTask(sleep);
 #endif
 }
 
