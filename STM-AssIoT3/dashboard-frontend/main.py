2a02307916a80eac6aa34f832907f495cea09400
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2025-02-17 21:29:59 +0100

    Edited Python Dashboard logic and pattern.

--- STM-AssIoT3/dashboard-frontend/main.py
+++ STM-AssIoT3/dashboard-frontend/main.py
@@ -1,12 +1,16 @@
 import tkinter as tk
-from gui import DashboardGUI
-from http_client import HTTPClient
+from view import Dashboard
+from model import HTTPClient
+from controller import Controller
 import config
 
 def main():
     root = tk.Tk()
-    http_client = "" # HTTPClient(config.BACKEND_URL) # TODO: da finire questa parte
-    DashboardGUI(root, http_client)
+    model = "" # HTTPClient(config.BACKEND_URL) # TODO: da finire questa parte
+    controller = Controller(model, None)
+    view = Dashboard(root, Controller)
+    controller.view = view
+
     root.mainloop()
 
 if __name__ == "__main__":
